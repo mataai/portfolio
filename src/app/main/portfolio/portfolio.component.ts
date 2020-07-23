486c5272da2d89590824d75dd3c98aae55ec39d9
HandsomeRomanian <mat4school@gmail.com>
2020-07-23 09:41:32 -0400

    Changed project sect to timeline

--- src/app/main/portfolio/portfolio.component.ts
+++ src/app/main/portfolio/portfolio.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    
+
+  }
 
   ngOnInit() {
   }
