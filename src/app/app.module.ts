9f8ca0120b7a45076ac5529298d2f2a0e1d2de2c
Matai <mat4school@gmail.com>
2023-06-29 21:36:35 -0400

    clean repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainModule } from './main/main.module';
 import { SepaqModule } from './sepaq/sepaq.module';
-import {ClipboardModule} from '@angular/cdk/clipboard';  
 
 @NgModule({
   declarations: [
--- src/app/main/portfolio/portfolio.component.ts
+++ src/app/main/portfolio/portfolio.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Repo } from './models';
 
 @Component({
   selector: 'app-portfolio',
@@ -8,13 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
+  public repos$: Observable<Repo[]>;
+
   constructor(private readonly _httpClient: HttpClient) { }
 
-    
-
+  ngOnInit() {
+    this.repos$ = this._httpClient.get<Repo[]>('https://api.github.com/users/handsomeromanian/repos');
+    this.repos$.pipe(tap(x => console.log(x)))
   }
 
-  ngOnInit() {
+  public goToUrl(url: string) {
+    window.open(url, '_blank');
   }
 
 }
+
